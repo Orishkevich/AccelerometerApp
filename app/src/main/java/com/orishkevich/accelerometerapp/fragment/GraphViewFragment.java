95ddbecb5aeaefa33680de1ae481e6db971fe83b
Stanislav Orishkevich <orishkevich.stanislav@gmail.com>
2017-05-11 12:56:10 +0300

    добавил графики

--- app/src/main/java/com/orishkevich/accelerometerapp/fragment/GraphViewFragment.java
+++ app/src/main/java/com/orishkevich/accelerometerapp/fragment/GraphViewFragment.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.orishkevich.accelerometerapp.R;
 
 
@@ -23,7 +26,24 @@ public class GraphViewFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_graph_view, container, false);
 
 
+
         return rootView;
     }
-
+    @Override
+    public void onViewCreated(View view,Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        GraphView graphX = (GraphView)getActivity().findViewById(R.id.graphx);
+        GraphView graphY = (GraphView)getActivity().findViewById(R.id.graphy);
+        GraphView graphZ = (GraphView)getActivity().findViewById(R.id.graphz);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graphX.addSeries(series);
+        graphY.addSeries(series);
+        graphZ.addSeries(series);
+    }
 }
